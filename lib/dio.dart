6e2f8d266680071badc1bd1d9ecda5a271f89e1a
Maximilian Schieder <maximilian.schieder@posteo.net>
2021-01-28 20:17:32 +0100

    Add easier initiation for dito

---
+++ lib/dio.dart
@@ -0,0 +1,13 @@
+import 'package:dio/dio.dart';
+
+Dio dio() {
+  var dio = Dio(BaseOptions(
+      baseUrl: "http://127.0.0.1:8000/api/",
+      responseType: ResponseType.plain,
+      headers: {
+        'accept': 'application/json',
+        'content-type': 'application/json'
+      }));
+
+  return dio;
+}
